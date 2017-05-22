d585ad9001903b6a43f7534c4e2e5ed5c9c6753a
nonzeronucleus <nonzeronucleus@ianplumb.org>
2017-05-22 22:14:17 +0100

    Added Redux dev tools

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import reducers from './reducers';
-import logo from './logo.svg';
 import './App.css';
 
 let store = createStore(reducers,
@@ -15,8 +14,7 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
+            <h2>Welcome to My React Pages</h2>
           </div>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
